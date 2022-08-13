62c466ada08ac614d480283380e53789db57998e
NSugam <neupane.sugam.19.sn@gmail.com>
2022-08-13 15:03:55 +0530

    Awesome Cart

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Product
+from math import ceil
 
 def index(request):
-    return render(request,'shop/index.html')
+    products= Product.objects.all()
+    n= len(products)
+    nSlides= n//4 + ceil((n/4) + (n//4))
+    params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
+    return render(request,"shop/index.html", params)
+
+def about(request):
+    return render(request,'shop/about.html')
+
+def contact(request):
+    return render(request, 'shop/contact.html')
+
+def tracker(request):
+    return HttpResponse("At Tracker")
+
+def search(request):
+    return HttpResponse("At Search")
+
+def productView(request):
+    return HttpResponse("At Product View")
+
+def checkout(request):
+    return HttpResponse("At Checkout")
+
